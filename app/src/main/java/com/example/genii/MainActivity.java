ec6bc39e310a77616348fd5d3c12b6e6f85cc6dc
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-03-30 10:01:59 -0400

    Create splash screen

--- app/src/main/java/com/example/genii/MainActivity.java
+++ app/src/main/java/com/example/genii/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;

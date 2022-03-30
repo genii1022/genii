f59af8f81c358bb64e1acf5943f23423bc597323
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-03-30 06:35:21 -0400

    Added basic toolbar

--- app/src/main/java/com/example/genii/MainActivity.java
+++ app/src/main/java/com/example/genii/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.genii;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
          BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
          navView.getMenu().findItem(R.id.placeholder).setEnabled(false);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }

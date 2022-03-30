a554ff4fd6cf7cd709119e041f99c0b3ea6c2a70
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-03-30 02:48:14 -0400

    Added basic navbar

---
+++ app/src/main/java/com/example/genii/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.genii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+         navView.getMenu().getItem(2).setEnabled(false);
+    }
+}

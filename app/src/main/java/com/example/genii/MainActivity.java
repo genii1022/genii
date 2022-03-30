d6c293a82669fd03eb04dfd19332550de70ca999
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-03-30 08:19:58 -0400

    Added functionality to the navigation menu and floating action button

--- app/src/main/java/com/example/genii/MainActivity.java
+++ app/src/main/java/com/example/genii/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.genii;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
-         navView.getMenu().findItem(R.id.placeholder).setEnabled(false);
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        navView.getMenu().findItem(R.id.placeholder).setEnabled(false);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        navView.setOnItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
+
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fab);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new AddFragment()).commit();
+                navView.setSelectedItemId(R.id.placeholder);
+            }
+        });
     }
+    private final NavigationBarView.OnItemSelectedListener navListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.miHome:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.miPlay:
+                            selectedFragment = new StartFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+                    return true;
+                }
+            };
 }

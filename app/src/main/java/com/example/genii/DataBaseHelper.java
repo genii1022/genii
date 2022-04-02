e5b75ff90170fc45d7952301e980abe032581dd4
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-04-02 12:20:24 -0400

    Added recycler view adapter and updated database helper class to fetch all cards

--- app/src/main/java/com/example/genii/DataBaseHelper.java
+++ app/src/main/java/com/example/genii/DataBaseHelper.java
@@ -2,10 +2,14 @@ package com.example.genii;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -40,5 +44,29 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         long insert = db.insert(CARDS_TABLE, null, cv);
 
         return insert != -1;
+    }
+
+    public List<CardModel> getAllCards() {
+        List<CardModel> returnList = new ArrayList<>();
+
+        String query = "SELECT * FROM " + CARDS_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String question = cursor.getString(1);
+                String answer = cursor.getString(2);
+
+                CardModel card = new CardModel(id, question, answer);
+                returnList.add(card);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return returnList;
     }
 }

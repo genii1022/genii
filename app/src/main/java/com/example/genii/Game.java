b90ef80edc31d43bff9e1cbb360e4b095ee2ece4
Ali Sultan <55068584+cybxr@users.noreply.github.com>
2022-04-02 19:00:39 -0400

    Added gameplay functionality

---
+++ app/src/main/java/com/example/genii/Game.java
@@ -0,0 +1,72 @@
+package com.example.genii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Game extends AppCompatActivity {
+    List<CardModel> cards;
+    int index;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+
+        findViewById(R.id.btn_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Game.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(Game.this);
+        cards = dataBaseHelper.getAllCards();
+
+        Collections.shuffle(cards);
+
+        index = 0;
+
+        TextView mainTxt = (TextView) findViewById(R.id.tv_mainText);
+        mainTxt.setText(cards.get(index).getQuestion());
+
+        TextView tip = (TextView) findViewById(R.id.tv_clickTip);
+
+        findViewById(R.id.game_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mainTxt.getText() == cards.get(index).getQuestion()) {
+                    mainTxt.setText(cards.get(index).getAnswer());
+                    tip.setText("Click anywhere on the card to show the answer");
+                } else {
+                    mainTxt.setText(cards.get(index).getQuestion());
+                    tip.setText("Click anywhere on the card to show the question");
+                }
+            }
+        });
+
+        findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (index >= cards.size()) {
+                    findViewById(R.id.btn_home).callOnClick();
+                    return;
+                }
+
+                index++;
+                mainTxt.setText(cards.get(index).getQuestion());
+                tip.setText("Click anywhere on the card to show the answer");
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/genii/StartFragment.java
+++ app/src/main/java/com/example/genii/StartFragment.java
@@ -1,5 +1,6 @@
 package com.example.genii;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,14 @@ public class StartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View view =  inflater.inflate(R.layout.fragment_start, container, false);
+        view.findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Game.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
